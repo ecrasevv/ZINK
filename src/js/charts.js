58abc1b5a6aeb31e28b932a3d66aa2cc65174972
Benny-001 <benedetta.zaccaro001@studenti.uniparthenope.it>
2024-11-25 17:35:28 +0100

    Add the profile view and the charts in the profile, charts still not working

---
+++ src/js/charts.js
@@ -0,0 +1,43 @@
+
+const ctx = document.getElementById('quizChart').getContext('2d');
+
+let savedData = JSON.parse(localStorage.getItem('charData')) || [0,0,0,0,0,0,0];
+
+const data = {
+	labels:['Sunday','Monday', 'Thursday', 'Wednesday', 'Tuesday', 'Friday','Saturday'],
+	datasets: [ {
+		label: 'Risposte Corrette',
+		data: savedData,
+		backgroundColor: 'rgba(153, 102, 255, 0.2)',
+		borderColor: 'rgba(153, 102, 255, 1)',
+		borderWidth: 1
+	}]
+};
+
+
+const options = {
+	scales: {
+		y: {
+			beginAtZero: true
+		}
+	}
+};
+
+let chart = new Chart(ctx, {
+	type: 'bar',
+	data: data,
+	options: options
+});
+
+
+function updateChart() {
+	let dayIndex = new Date().getDay();
+	
+	data.datasets[0].data[dayIndex]++;
+	chart.update();
+	
+	localStorage.setItem('charData',JSON.stringify(data.datasets[0].data));
+	
+	console.log('Dati salvati e aggiornati:', data.datasets[0].data);
+}
+
