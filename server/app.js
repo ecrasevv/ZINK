9471ab01ef301a15839701166f4cab04c214e1cf
ecrasevv <francesco.ostuni001@studenti.uniparthenope.it>
2024-12-01 17:15:28 +0100

    feat: starter page + handling POST req

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('../src'));
+// handle GET -> serve the starter page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/login.html'));
+});
+
+// handle CSS, img, HTML
+app.use(express.static(path.join(__dirname, '../src')));
+
+// handle POST 
+app.post('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
 
 app.listen(3000, () => {
-    console.log("app listening on port 3000");
-})
+    console.log("App listening on port 3000");
+});
+
