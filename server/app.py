2b1dbc0d47758dd16ff5d3e943ebe4023e62b594
ecrasevv <francesco.ostuni001@studenti.uniparthenope.it>
2024-12-13 09:44:58 +0100

    batman

---
+++ server/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, send_from_directory, request
+
+app = Flask(__name__, static_folder='../src')
+
+# Handle GET -> serve the starter page
+@app.route('/')
+def home():
+    return send_from_directory('../src', 'login.html')
+
+# Handle POST
+@app.route('/login', methods=['POST'])
+def login():
+    return send_from_directory('../src', 'index.html')
+
+# Serve static files (CSS, images, HTML)
+@app.route('/<path:filename>')
+def static_files(filename):
+    return send_from_directory('../src', filename)
+
+if __name__ == '__main__':
+    app.run(port=3000)
